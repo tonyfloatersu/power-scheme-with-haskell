db4756613956b78b0fb9acdd3b2f9e165e7a5b4c
tonyfloatersu <tonyfloater@gmail.com>
2018-03-20 04:30:30 +0800

    add new parse func

--- src/C2/Notes.hs
+++ src/C2/Notes.hs
@@ -9,15 +9,15 @@ symbol    = oneOf "!$%|*+-/:<=?>@^_~#"
 spaces :: Parser ()
 spaces    = skipMany1 space
 
-callParserExample :: String -> String
-callParserExample input    = case parse (spaces >> symbol) "lisp" input of
-                                 Left err -> "No match: " ++ show err
-                                 Right _  -> "Found value"
+callParserExample :: Parser a -> String -> String
+callParserExample func input    = case parse func "lisp" input of
+                                      Left err -> "No match: " ++ show err
+                                      Right _  -> "Found value"
 
 sampleParserIO :: IO ()
 sampleParserIO    =  getArgs
                  >>= pure . head . take 1
-                 >>= (putStrLn . callParserExample)
+                 >>= (putStrLn . callParserExample (spaces >> symbol))
 
 data LispVal = Atom String
              | List [LispVal]
@@ -25,3 +25,9 @@ data LispVal = Atom String
              | Number Integer
              | String String
              | Bool Bool
+
+parseString :: Parser LispVal
+parseString    =  char '"'
+              >>  many (noneOf "\"")
+              >>= \x -> char '"'
+              >>= \_ -> return $ String x
