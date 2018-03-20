1c348cdf3ebebceb60e5d03f40a832911e587656
tonyfloatersu <tonyfloater@gmail.com>
2018-03-20 08:17:31 +0800

    add C2E2 string parser

--- src/C2/C2E1.hs
+++ src/C2/C2E1.hs
@@ -1,3 +1,7 @@
 module C2.C2E1 where
 
 import           Text.ParserCombinators.Parsec hiding (spaces)
+import           C2.Notes (LispVal (..))
+
+parseNumber :: Parser LispVal
+parseNumber    = many1 digit >>= pure . Number . read
---
+++ src/C2/C2E2.hs
@@ -0,0 +1,16 @@
+module C2.C2E2 where
+
+import           Text.ParserCombinators.Parsec hiding (space)
+import           C2.Notes (LispVal (..))
+
+parseString :: Parser LispVal
+parseString    =  char '"'
+              >>  (many $ many1 (noneOf "\"\\")
+              <|>        escapeCharacter)
+              >>= \x -> char '"'
+              >>= \_ -> (pure . String . concat) x
+
+escapeCharacter :: Parser String
+escapeCharacter    =  char '\\'
+                  >>  oneOf "\\\""
+                  >>= pure . (: [])
